ba7e6417ef88faf45af31d030c7ecdf61f235db4
satyamsaty <satyam.saty9@gmail.com>
2019-12-30 22:37:54 +0530

    Working with numbers

---
+++ Third.py
@@ -0,0 +1,24 @@
+print (3 + 2)
+print ( 3 -2)
+print ( 3 * 2)
+print (3 / 2)
+print (3 // 2)
+print ( 3 % 2)
+num_1 = 3
+num_2 = 4
+print (num_1 == num_2)
+print (num_1 != num_2)
+print (num_1 < num_2)
+print (num_1 >= num_2)
+
+num1 = '100'
+num2 = '200'
+print (num1 + num2)
+
+num1 = int(num1)
+num2 = int (num2)
+print (num1 + num2)
+
+num3 = 'satyam'
+num4 = 'kumar'
+print (num3 + num4)
