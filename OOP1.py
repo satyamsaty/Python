916ff6a51584f02296d15e28abde2563d2f00668
satyamsaty <satyam.saty9@gmail.com>
2020-01-05 10:20:30 +0530

    Fisrt with OOP basics

---
+++ OOP1.py
@@ -0,0 +1,28 @@
+
+class Employee:
+    pass
+
+
+
+
+emp1=Employee()
+emp1.first='satyam'
+emp1.last='kumar'
+emp1.job='sonus'
+emp1.salary=60000
+print(emp1.first)
+print(emp1.last)
+print(emp1.job)
+print(emp1.salary)
+
+print(emp1)
+emp2=Employee()
+print(emp2)
+emp2.first='Shivam'
+emp2.last='kumar'
+emp2.job='google'
+emp2.salary=200000
+print(emp2.first)
+print(emp2.last)
+print(emp2.job)
+print(emp2.salary)
